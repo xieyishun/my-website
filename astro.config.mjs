518bfa5ea25524b0c46a5fb3c57e2316a307c95e
xieyishun <xieyishun@126.com>
2026-01-11 23:00:53 -0500

    first update

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  // 1. 你的 GitHub 主页地址
+  site: 'https://xieyishun.github.io', 
+  
+  // 2. 你的仓库名（必须以斜杠开头）
+  base: '/my-website', 
+});
