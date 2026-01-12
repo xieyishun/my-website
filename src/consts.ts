e1027327210c388342e6a6743e138d7cdf93f5df
xieyishun <xieyishun@126.com>
2026-01-12 05:07:12 -0500

    update homepage to Chinese

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = '刀刀 | 数据分析';
+export const SITE_DESCRIPTION = '欢迎来到刀的运营笔记';
