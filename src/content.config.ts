faa1f9ec9931092fa380e5e0b944c370a747db6e
xieyishun <xieyishun@126.com>
2026-01-30 03:03:34 -0500

    Update site components and fix fonts

--- src/content.config.ts
+++ src/content.config.ts
@@ -3,7 +3,7 @@ import { glob } from 'astro/loaders';
 
 const blog = defineCollection({
 	// Load Markdown and MDX files in the `src/content/blog/` directory.
-	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
+	loader: glob({ pattern: '**/*.{md,mdx}', base: './src/content/blog' })
 	// Type-check frontmatter using a schema
 	schema: ({ image }) =>
 		z.object({
@@ -18,7 +18,6 @@ const blog = defineCollection({
 			pubDate: z.coerce.date(),
 			updatedDate: z.coerce.date().optional(),
 			heroImage: image().optional(),
-			tags: z.array(z.string()).optional(),
 		}),
 });
 
